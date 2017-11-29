f21080e5da695d20ab15845d3da9e550dacb6752
rforzisi <rossfz02@gmail.com>
2017-11-29 14:07:29 +1100

    Started adding graphs to the history page

--- Exercise tracker/Exercise tracker/ViewModels/HistoryPageViewModel.cs
+++ Exercise tracker/Exercise tracker/ViewModels/HistoryPageViewModel.cs
@@ -13,10 +13,36 @@ namespace Exercise_tracker.ViewModels
 {
     public class HistoryPageViewModel : ObservableObject, IModalDialogViewModel
     {
-        public SeriesCollection GraphData { get; set; } //i think this needs to stay public so the graph can see it
+        public SeriesCollection GraphData { get; set; } 
         public List<string> XLabels { get; set; }
-        public Separator Seperator { get; set; }
+        //public List<object> GraphsToShow { get; set; } //List of object seems kinda bad. See if theres a better way
+        public Dictionary<string,object> AvailableGraphs { get; set; } //should probably be a cast object
+        public string XAxisTitle { get; set; }
+        public string YAxisTitle { get; set; }
+        public bool XSeperatorEnabled { get; set; }
 
+        private KeyValuePair<string, object> selectedGraph;
+        public KeyValuePair<string, object> SelectedGraph {
+            get { return selectedGraph; }
+            set {
+                if (value.Key != selectedGraph.Key)
+                {
+                    selectedGraph = value;
+                    //do something special here to show the graphs
+
+                    if (selectedGraph.Key == "All Exercise Counts")
+                    {
+                        ShowAllExerciseCounts();
+                    }
+                    else
+                    {
+                        ShowExerciseHistory(selectedGraph.Value as ExerciseItem); //kinda dangerous but as long as we pay attention to what we add, it should be alright
+                    }
+
+                }
+
+            }
+        } //Using a string is bad
         private bool? dialogResult;
         
         private List<ExerciseItem> AllExerciseItems;
@@ -24,33 +50,28 @@ namespace Exercise_tracker.ViewModels
 
         public HistoryPageViewModel (DataStore dataStore)
         {
-            AllExerciseItems = dataStore.LoadAllExerciseItems(); //probs doesnt need to be done again, but it makes the constructor simpler
+            AllExerciseItems = dataStore.LoadAllExerciseItems(); //probs doesnt need to be loaded again, but it makes the constructor simpler
             this.datastore = dataStore;
 
-            
+            AvailableGraphs = new Dictionary<string, object>();
 
-            //Random rng = new Random();
+            foreach (var exercise in AllExerciseItems)
+            {
+                AvailableGraphs.Add(exercise.ExerciseName,exercise);
+            }
+            AvailableGraphs.Add("All Exercise Counts",null);
 
-            //TestData.Add(new LineSeries()
-            //{
-            //    Values = new ChartValues<ObservableValue>()
-            //});
-
-            //for (int i = 0; i < 100; i++)
-            //{
-            //    TestData[0].Values.Add(rng.Next());
-            //    //TestData[1].Values.Add(new ObservableValue(rng.Next()));
-            //}
             //ShowExerciseHistory(AllExerciseItems.FirstOrDefault());
-            
             ShowAllExerciseCounts();
+            selectedGraph = new KeyValuePair<string, object>("All Exercise Counts", null);
         }
 
         //Each type of show graph function will have to set up the graph on its own
         private void ShowExerciseHistory(ExerciseItem exercise)
         {
             //for this mode, we need to setup the graph so that it will display a fixed set of days on the graph(scatter plot)
-
+            XAxisTitle = exercise.ExerciseName;
+            YAxisTitle = "Day of the Year completed (This needs to change)";
             GraphData = new SeriesCollection();
 
             GraphData.Add(new ScatterSeries()
@@ -58,29 +79,43 @@ namespace Exercise_tracker.ViewModels
                 Values = new ChartValues<int>()
             });
 
-            
+            XLabels = new List<string>();
+            XSeperatorEnabled = true;
 
-            var allHistory = datastore.LoadExerciseHistory(exercise);
+            List<ExerciseHistoryItem> allHistory = datastore.LoadExerciseHistory(exercise);
+
+            allHistory = allHistory.OrderBy(x => x.TimeCompleted).ToList();
+
+            //DateTime dateStart = allHistory.FirstOrDefault().TimeCompleted;
+            //DateTime dateEnd = allHistory.LastOrDefault().TimeCompleted;
+
+            //while (dateStart.DayOfYear != dateEnd.DayOfYear) //this will obviously not work when the year changes
+            //{
+            //    XLabels.Add(dateStart.ToShortDateString());
+            //    dateStart += new TimeSpan(1,0,0,0);
+            //}
 
             foreach (var his in allHistory)
             {
+                XLabels.Add(his.TimeCompleted.ToShortDateString());
                 GraphData[0].Values.Add(his.TimeCompleted.DayOfYear);
-                
             }
+
+            VisualUpdateGraph();
         }
 
-        private void ShowAllExerciseCounts() 
+        private void ShowAllExerciseCounts()
         {
+            XAxisTitle = "Exercises";
+            YAxisTitle = "Count";
             GraphData = new SeriesCollection();
 
             GraphData.Add(new ColumnSeries());
             GraphData[0].Values = new ChartValues<int>();
 
-            Seperator = new Separator(){IsEnabled = false};
-
+            XSeperatorEnabled = false;
 
             XLabels = new List<string>();
-
 
             foreach (var exerciseItem in AllExerciseItems)
             {
@@ -89,6 +124,8 @@ namespace Exercise_tracker.ViewModels
                 GraphData[0].Values.Add(history.Count);
                 XLabels.Add(exerciseItem.ExerciseName);
             }
+
+            VisualUpdateGraph();
         }
 
         public bool? DialogResult
@@ -100,5 +137,15 @@ namespace Exercise_tracker.ViewModels
                 RaisePropertyChangedEvent("DialogResult");
             }
         }
+
+        private void VisualUpdateGraph()
+        {
+            RaisePropertyChangedEvent("GraphData");
+            RaisePropertyChangedEvent("XAxisTitle");
+            RaisePropertyChangedEvent("YAxisTitle");
+            RaisePropertyChangedEvent("XLabels");
+            RaisePropertyChangedEvent("XSeperatorEnabled");
+        }
+
     }
 }
